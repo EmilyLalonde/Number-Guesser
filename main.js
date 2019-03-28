78bb530b470183f08c9b12d948ff0ab762403963
Vinton Teo <taulago@ymail.com>
2019-03-27 20:50:29 -0600

    Refactor error message functions in main.js and made number inputs only accept numbers in index.html

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // Variables and Named Functions
 
 var magicNum;
-
 var minRangeInput = document.querySelector('#min-range-input');
 var maxRangeInput = document.querySelector('#max-range-input');
 var updateButton = document.querySelector('#update-button');
@@ -22,9 +21,9 @@ var minRangeError = document.querySelector(".min-range-error-message");
 var maxRangeError = document.querySelector(".max-range-error-message");
 var playerOneGuessOutside = document.querySelector(".player-one-guess-outside");
 var playerTwoGuessOutside = document.querySelector(".player-two-guess-outside");
-
 var integerMin = parseInt(minRangeInput.value);
 var integerMax = parseInt(maxRangeInput.value);
+
 //Random Number Generator
 function RandomNum(x,y) {
   magicNum = Math.floor(Math.random() * (y - x + 1) + x);
@@ -45,23 +44,20 @@ console.log("start random number is " + magicNum);
 //Set Range Section
 
 function setRange() {
-  var regex = /^[0-9]+$/;
+  console.log("update button fired...setting range...")
   var xx = minRangeInput.value;
   var yy = maxRangeInput.value;
-  var regex = /^[0-9]+$/;
-  var numberCheckerMin = xx.match(regex);
-  var numberCheckerMax = yy.match(regex);
   setMinRange.innerText = xx;
   setMaxRange.innerText = yy;
   RandomNum(parseInt(setMinRange.innerText),parseInt(setMaxRange.innerText));
-  minRangeErrorMessage();
-  maxRangeErrorMessage();
-  console.log(`range set from ${setMinRange.innerText} to ${setMaxRange.innerText}`);
+  rangeErrorMessage();
+  console.log(`range set; minimum ${setMinRange.innerText} to maximum ${setMaxRange.innerText}`);
   console.log(`the random number is now ${magicNum}`)
   updateButton.disabled = true;
 }
 
 function checkForm() {
+  console.log("running checkForm function")
   var rangeForms = document.querySelectorAll('.ranger');
   var gameInputs = document.querySelectorAll('.game-inputs')
   var canSubmit = true;
@@ -82,48 +78,45 @@ function checkForm() {
 }
 
 
-function minRangeErrorMessage() {
-  console.log('checking minRange for error')
+function rangeErrorMessage() {
+  console.log('checking range for error');
+  console.log(parseInt(minRangeInput.value), parseInt(maxRangeInput.value));
   if (parseInt(minRangeInput.value) > parseInt(maxRangeInput.value) ) {
-    console.log("minRange error triggered");
+    console.log("range error triggered");
     minRangeError.innerText = 'Error: Minimum needs to be less than maximum!';
+    maxRangeError.innerText = 'Error: Maximum needs to be more than minimum!';
+    clearInputs();
+    setMinRange.innerText = 1;
+    setMaxRange.innerText = 100;
+    RandomNum(1,100);
   } else {
     minRangeError.innerText = '';
-  }
-}
-
-function maxRangeErrorMessage() {
-  console.log('checking maxRange for error')
-  if (parseInt(minRangeInput.value) > parseInt(maxRangeInput.value) ){
-    console.log("maxRange error triggered");
-    maxRangeError.innerText = 'Error: Maximum needs to be more than minimum!';
-  } else {
     maxRangeError.innerText = ''; 
   }
 }
 
+
+
 // Game Section
 
-//input guard 
-
-
-
 function clearInputs() {
-  for (i = 0; i < document.querySelectorAll('form').length; i++) {
-    document.querySelectorAll('form')[i].reset();
+  for (i=0; i < document.querySelectorAll('form').length; i++) {
+  document.querySelectorAll('form')[i].reset();
   }
+  clearButton.disabled = true;
 }
 
 function startGame(p1input,p1guess,p2input,p2guess) {
   var playerOneCompass = document.querySelector('#compass-one');
   var playerTwoCompass = document.querySelector('#compass-two');
-  console.log(minRangeInt);
-  console.log(maxRangeInt);
-  console.log(parseInt(playerOneGuess.value));
-  if (playerOneGuess.value < minRangeInt || playerOneGuess > maxRangeInt || playerTwoGuess.value < minRangeInt || playerTwoGuess.value > maxRangeInt) {
+  var pOneGuess = parseInt(playerOneGuess.value);
+  var pTwoGuess = parseInt(playerTwoGuess.value);
+  if (pOneGuess < minRangeInt || pOneGuess > maxRangeInt || pTwoGuess < minRangeInt || pTwoGuess > maxRangeInt) {
     console.log("error check working");
-    outsideRangeOne();
-    outsideRangeTwo();
+    outsideRangeError();
+  } else {
+    playerOneGuessOutside.innerText = "";
+    playerTwoGuessOutside.innerText = "";
   }
   for (i = 0; i < challengerOneTags.length; i++) {
     challengerOneTags[i].innerText = p1input;
@@ -157,26 +150,12 @@ function startGame(p1input,p1guess,p2input,p2guess) {
 }
 
 
-function outsideRangeOne() {
+function outsideRangeError() {
     var pOneGuess = parseInt(playerOneGuess.value);
     var minInputInt = parseInt(minRangeInput.value);
     var maxInputInt = parseInt(maxRangeInput.value);
-  if (pOneGuess < minInputInt || pOneGuess > maxInputInt){
     playerOneGuessOutside.innerText = 'Error: Number must be within range!';
-  }else {
-    playerOneGuessOutside.innerText = '';
-  }
-}
-
-function outsideRangeTwo() {
-  var pTwoGuess = parseInt(playerTwoGuess.value);
-    var minInputInt = parseInt(minRangeInput.value);
-    var maxInputInt = parseInt(maxRangeInput.value);
-  if (pTwoGuess < minInputInt || pTwoGuess > maxInputInt){
     playerTwoGuessOutside.innerText = 'Error: Number must be within range!';
-  }else {
-    playerTwoGuessOutside.innerText = '';
-  }
 }
 
 // // -- Event Listeners
@@ -187,18 +166,12 @@ updateButton.addEventListener('click', setRange)
 submitButton.addEventListener('click', function() {
   console.log("submit button pressed");
   startGame(playerOneInput.value, playerOneGuess.value, playerTwoInput.value, playerTwoGuess.value);
-  outsideRangeOne();
-  outsideRangeTwo();
 })
 
-clearButton.addEventListener('click', function() {
-  for (i=0; i < document.querySelectorAll('form').length; i++) {
-  document.querySelectorAll('form')[i].reset();
-  }
-  clearButton.disabled = true;
-})
+clearButton.addEventListener('click', clearInputs)
 
 resetButton.addEventListener('click', function() {
+  console.log("reset button triggered")
   setMinRange.innerText = 1;
   setMaxRange.innerText = 100;
   for (i = 0; i < challengerOneTags.length; i++) {
@@ -211,6 +184,8 @@ resetButton.addEventListener('click', function() {
   };
   playerOneResult.innerText = "00";
   playerTwoResult.innerText = "00";
+  minRangeError.innerText = '';
+  maxRangeError.innerText = '';
   clearInputs();
   RandomNum(1,100);
   console.log(magicNum);
